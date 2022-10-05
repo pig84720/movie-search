f9b05eae5f08f2cbfa1678f49b4d99e387108fea
luke.dong <LUKEDONG@hotaimotor.com.tw>
2022-10-05 10:57:03 +0800

    初步串接TMDB API

--- app/appConfig.js
+++ app/appConfig.js
@@ -31,26 +31,8 @@ SS.namespace("SS.ssapi.url");
 SS.namespace("SS.image.baseUrl");
 SS.namespace("SS.apiKey");
 
-<<<<<<< HEAD
 SS.service.baseUrl = "https://api.themoviedb.org/3/movie/";
-SS.app.baseUrl = "/";
+SS.app.baseUrl = "/movie-search/";
 SS.image.baseUrl = "https://image.tmdb.org/t/p/w500";
 SS.apiKey = "2b6190c7ca4e58aac8a43283286cbce7";
-=======
-SS.service.baseUrl = "https://ap.searching-service.com/SSService/";
-// if (location.host == '') {
-//     SS.app.baseUrl = location.href.replace('file://', '').substr(0, location.href.replace('file://', '').indexOf('/www/') + 5);
-// } else {
-//     var baseUrlArray = location.href.split(location.hostname + (location.port != '' ? ':' + location.port : '') + '/');
-//     SS.app.baseUrl = '/' + (baseUrlArray.length > 1 ? (baseUrlArray[1].indexOf('/') > -1 ? baseUrlArray[1].split('/')[0] + '/' : baseUrlArray[1]) : '');
-// }
-SS.app.baseUrl = "/movie-search/";
-SS.portal.serverInfo = [
-    'SSSQYHFC02',
-    'PP',
-    '',
-    '',
-    'S'
-];
->>>>>>> 988b3faaacd271375f6615b2df8ec1e45b84dabb
 SS.ssapi.token='VfaU+LJXyYZp7Nr3mFhCQtBfZ/rL2AQmOjkOW4W1uZVumEKn0wIHcD/RsdkmgB8di2Y9HFgUS/7HFxHm4m9eACLvfBCTdBEGoGqcd6RDUeZNSwlOrVeFarS9bEalGyz6';
